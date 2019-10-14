706ca06a2f999696d7a23fde355ab832cebf4bf3
andy <andytan727@gmail.com>
2019-10-14 09:52:38 +0800

    Uploaded

---
+++ src/containers/userimage.js
@@ -0,0 +1,61 @@
+import React from "react";
+import LoadingIndicator from "../components/LoadingIndicator";
+import axios from "axios";
+import Image from "react-graceful-image";
+
+class UserImages extends React.Component {
+  state = {
+    userImages: [],
+    isLoading: true,
+    userId: null
+  };
+
+  componentDidMount() {
+    const userId = this.props.userId;
+    // GET request to Nextagram API
+    // console.log(userId);
+    axios
+      .get(`https://insta.nextacademy.com/api/v1/images?userId=${userId}`)
+      .then(result => {
+        // console.log(result.data);
+        // If successful, load users array with profile data
+        this.setState({
+          userImages: result.data,
+          isLoading: false,
+          userId: userId
+        });
+      })
+      .catch(error => {
+        // If unsuccessful, console log error
+        console.log("ERROR: ", error);
+      });
+  }
+
+  render() {
+    const { isLoading, userImages } = this.state;
+    return (
+      <>
+        <div className="col">
+          <div className="row">
+            {userImages.map((image, index) => (
+              <div key={index} className="col-sm-4 mb-2">
+                <Image
+                  src={image}
+                  className="img-fluid"
+                  width="400"
+                  height="400"
+                  alt="userImage"
+                  retry={{ count: 10, delay: 2 }}
+                  key={index}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        {isLoading ? <LoadingIndicator /> : null}
+      </>
+    );
+  }
+}
+
+export default UserImages;
